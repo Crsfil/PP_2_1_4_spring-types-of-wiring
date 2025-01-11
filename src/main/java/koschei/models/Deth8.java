8b8d5aa82f14ed2142a33d6c96a1e2176036e67c
Crsfil <strizhovhotmail@gmail.com>
2025-01-11 17:21:46 +0300

    готово

--- src/main/java/koschei/models/Deth8.java
+++ src/main/java/koschei/models/Deth8.java
@@ -1,4 +1,17 @@
 package koschei.models;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Deth8 {
+    private String deth8 = "";
+
+    public void setDeth8(String deth8) {
+        this.deth8 = deth8;
+    }
+
+    @Override
+    public String toString() {
+        return deth8;
+    }
 }
