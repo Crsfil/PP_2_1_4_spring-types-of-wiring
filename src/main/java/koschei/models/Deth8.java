4ab2cdd1f154376274ce88605526484accad5da1
Crsfil <strizhovhotmail@gmail.com>
2025-01-14 10:23:24 +0300

    deth8 через сеттер, убрал лишние аннотации, добавил соединение чисто через поле без конструктора

--- src/main/java/koschei/models/Deth8.java
+++ src/main/java/koschei/models/Deth8.java
@@ -1,14 +1,18 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Deth8 {
     private String deth8 = "";
 
-    public void setDeth8(String deth8) {
-        this.deth8 = deth8;
+    public String getDeth8() {
+        return deth8;
     }
+
+    @Autowired
+    public void setDeth8(Deth8 deth8) { this.deth8 = String.valueOf(deth8); }
 
     @Override
     public String toString() {
